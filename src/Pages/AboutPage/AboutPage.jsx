8742f5615f1b52b1b476301e69ac58b3b2ce06eb
Maria Sui <maria.sui51@gmail.com>
2023-08-22 13:16:29 -0400

    changed styling

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../../Components/Header/Header'
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <>
+      <Header/>
+    </>
   )
 }
 
