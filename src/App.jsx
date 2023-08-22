b98f5517b88760cadac4404947cf46345540075e
Maria Sui <maria.sui51@gmail.com>
2023-08-22 11:26:20 -0400

    added html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import AboutPage from './Pages/AboutPage/AboutPage';
 import ChatPage from './Pages/ChatPage/ChatPage';
 import HomePage from './Pages/HomePage/HomePage';
-import LoginPage from './Pages/LoginPage/LoginPage';
 import SelectionsPage from './Pages/SelectionsPage/SelectionsPage';
 import './app.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -11,7 +10,7 @@ function App() {
    <BrowserRouter>
     <Routes>
       <Route path='/' element={<HomePage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/login' element={<HomePage/>}/>
       <Route path='/about' element={<AboutPage/>}/>
       <Route path='/selections' element={<SelectionsPage/>}/>
       <Route path='/selections/:id' element={<SelectionsPage/>}/>
--- src/Components/HomePageMain/HomePageMain.jsx
+++ src/Components/HomePageMain/HomePageMain.jsx
@@ -6,15 +6,34 @@ import { useState } from 'react';
 
 
 function HomePageMain() {
-
+    const [login, setLogin]=useState(false);
+    const clickHandlerChange=()=>{
+        setLogin(true);
+    }
   return (
     <>
         <main className='homePage'>
             <img className='homePage__pic' src={HomePagePic} alt='people dancing'></img>
             <div className='homePage__wrapper--right'>
                 <div className='homePage__wrapper--right--top'>
-                    <Link className='homePage__link--login' to='/login'>
-                        <h1 className='homePage__title'>Do you miss times like this?</h1>
+                    <Link className={!login ? 'homePage__link--login homePage__link--login--glow' : 'homePage__link--login'} to='/login' onClick={clickHandlerChange}>
+                        {!login ? <h1 className='homePage__title'>Do you miss times like this?</h1>
+                        : <form className='homePage__login-form'>
+                            <div>
+                            <label className='homePage__login-text'>Username :</label>
+                            <input className='homePage__login-input' type='text' placeholder='create username...'></input><br></br>
+                            </div>
+                            <div>
+                            <label className='homePage__login-text'>Are you at least 18 years old?</label><br></br>
+                            </div>
+                            <div>
+                            <input className='homePage__login-checkbox' type='checkbox' name='yes' id='yes'></input>
+                            <label className='homePage__login-text' for='yes'>Yes</label>
+                            <input className='homePage__login-checkbox' type='checkbox'></input>
+                            <label className='homePage__login-text'>No</label><br></br>
+                            </div>
+                            <button className='homePage__login-button'>Log in</button>
+                          </form>}
                     </Link>
                 </div>
                 <div className='homePage__wrapper--right--bottom'>
