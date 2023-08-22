a1483df2e8c0c68620618be8c418ffb5ac40c70c
Maria Sui <maria.sui51@gmail.com>
2023-08-22 19:34:14 -0400

    added some html and styling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import icon from '../../Assets/Icons/icons8-dance-60.png'
+import './header.scss';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <div className="header__wrapper">
+            <div className="header__wrapper--left">
+                <h1 className='header__title'>You Should be Dancing</h1>
+                <img className='header__icon' src={icon} alt='figures dancing'></img>
+            </div>
+            <div className="header__wrapper--right">
+                <button className='header__button'>Log In</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
